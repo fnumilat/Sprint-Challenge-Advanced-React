18dfe68b6149054f32851cbf671030f0bdb2283b
unknown <fnumilat@gmail.com>
2020-02-22 12:40:30 -0600

    work progressed

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import PlayersCard from './Components/PlayersCard';
+import NavBar from './Components/NavBar';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      this.setState({players: res})
+    })
+    .catch(err => console.log('No Data', err))
+  }
+
+
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <PlayersCard players={this.state.players}/>
     </div>
-  );
+  )};
 }
 
 export default App;
