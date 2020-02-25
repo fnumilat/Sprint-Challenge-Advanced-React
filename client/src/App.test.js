d37997f25e39ec53ba292091c5e3f4f59da26d4c
unknown <fnumilat@gmail.com>
2020-02-24 19:01:44 -0600

    new tests written

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,6 +4,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 import NavBar from './Components/NavBar';
 import PlayersCard from './Components/PlayersCard';
+import UseDarkMode, { darkMode, setDarkMode} from './Hooks/UseDarkMode';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -19,4 +20,16 @@ test('renders Nav Bar without crashing', () => {
 test('renders Nav Bar title without crashing', () => {
   const { getByText } = render(<NavBar/>);
   getByText(/List of Women's World Cup players ranked by search interest from Google Trends, June-July 2019/i);
+});
+
+describe("the darkMode state is working well", ( ) => {
+  it("making sure the darkMode state is working", () => {
+    expect(darkMode).toBeInTheDocument;
+  })
+});
+
+describe("the setDarkMode state is working well", ( ) => {
+  it("making sure the setDarkMode state is working", () => {
+    expect(setDarkMode).toBeInTheDocument;
+  })
 });
